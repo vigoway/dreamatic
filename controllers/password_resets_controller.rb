5c801985fd2cacda7c9821d2990f54626c4a8d0d
vigoway <fmivigo@gmail.com>
2014-08-18 18:32:22 -0500

    Update password_resets_controller.rb

--- controllers/password_resets_controller.rb
+++ controllers/password_resets_controller.rb
@@ -2,27 +2,31 @@ class PasswordResetsController < ApplicationController
   def new
   end
 
+  # if a user forgot their password, send reset info to their email 
   def create
-  user = User.find_by_email(params[:email])
-  user.send_password_reset if user
-  flash[:notice] = "Email sent with password reset instructions."
-  render 'new' 
+    user = User.find_by_email(params[:email])
+    user.send_password_reset if user
+    flash[:notice] = "Email sent with password reset instructions."
+    render 'new' 
   end
 
-def edit
-  @user = User.find_by_password_reset_token!(params[:id])
-end
-
-def update
-  @user = User.find_by_password_reset_token!(params[:id])
-  if @user.password_reset_sent_at < 2.hours.ago
-    redirect_to new_password_reset_path, :alert => "Password reset has expired."
-  elsif @user.update_attributes(params[:user])
-    redirect_to root_url, :notice => "Password has been reset!"
-  else
-    render :edit
+  #user comes back through the link sent by the action above
+  def edit
+    @user = User.find_by_password_reset_token!(params[:id])
   end
-end
+
+  #updates the user's password at their request
+  def update
+    @user = User.find_by_password_reset_token!(params[:id])
+    #the user has to change their password within 2 hours after receiving the reset info
+    if @user.password_reset_sent_at < 2.hours.ago 
+      redirect_to new_password_reset_path, :alert => "Password reset has expired."
+    elsif @user.update_attributes(params[:user])
+      redirect_to root_url, :notice => "Password has been reset!"
+    else
+      render :edit
+    end
+  end
 
 
   
